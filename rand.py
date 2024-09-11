3eb356e2f61a64ec295bb2bba2600886104c550b
Raghunandan Mante <42904941+raghunandan-16@users.noreply.github.com>
2024-09-11 06:36:36 +0000

    added linting changes and its badge

--- rand.py
+++ rand.py
@@ -1,10 +1,17 @@
+"""
+This is python code to generate random array for input to merge sort
+"""
+
 import subprocess
 
 
 def random_array(arr):
+    """
+    This is random function to generate random array for input to merge sort
+    """
     shuffled_num = None
-    for i in range(len(arr)):
+    for i in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr
